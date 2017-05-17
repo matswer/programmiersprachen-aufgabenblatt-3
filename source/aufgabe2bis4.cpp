4230958cc76b39f59d3189f6e224cb3033371be7
mats werchohlad <mats.werchohlad@uni-weimar.de>
2017-05-17 17:03:26 +0200

    Aufgabe 3.3

--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -1,7 +1,9 @@
 #include <cstdlib> // std :: rand (), RAND_MAX 
 #include <list> // std :: list <>
 #include <vector> // std :: vector <>
+#include <set> //set
 #include <iostream> // std :: cout
+#include <string> // std::set <>
 #include <iterator> // std :: ostream_iterator <>
 #include <algorithm> // std :: reverse , std :: generate
 	
@@ -12,9 +14,29 @@ int main ()
       r = std :: rand ()% 101;
         }
     std :: copy (std :: begin (l1), std :: end(l1),
-              std :: ostream_iterator<int>( std :: cout , "\n"));
+            std :: ostream_iterator<int>( std :: cout , "\n"));
 
     std :: vector<unsigned int> v1(l1.size());
     std :: copy (std :: begin (l1), std :: end(l1), std :: begin (v1));
+    
+// Aufgabe 3.2
+    std :: set <unsigned int> set1(std :: begin (l1), std :: end(l1));
+    std :: copy (std :: begin (set1), std :: end(set1),
+            std :: ostream_iterator<int>( std :: cout , "\n"));
+    
+    std :: cout << "Anzahl der verschiedenen Zahlen: " << set1.size() << "\n";
+
+    std :: cout << "Die Zahlen die nicht enthalten sind lauten:\n";
+
+   for (int i=0; i<100; ++i){
+
+    if (set1.count(i)==0){
+    
+        std::cout << i << "\n";
+        }
+      
+    else
+    i=i+1;
+    };
     return 0;
-};
+    };
